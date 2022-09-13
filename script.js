d127412accaa6c92e6c8c77fba697a85202c47a1
ReemMohamedAbdelfatah <reem.mohamed.abdelfatah@gmail.com>
2022-09-13 13:53:30 +0200

    Completing hamburger menu

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-const mobileNav = document.querySelector(".mobile-nav");
+const mobileNav = document.querySelector('.mobile-nav');
+const body = document.getElementsByTagName('body')[0];
+const hamburgerclose = document.getElementById('close-icon');
+const hamburgeropen = document.getElementById('menu-icon');
+const hamburgerListItem = document.getElementsByClassName('mobile-nav-item');
 function openMenu() {
   mobileNav.style.visibility = 'visible';
+  body.style.overflow = 'hidden';
 }
 function closeMenu() {
   mobileNav.style.visibility = 'hidden';
-  window.setTimeout(() => {
-    window.location.reload();
-  }, 5000);
+  body.style.overflow = 'auto';
+}
+hamburgerclose.addEventListener('click', closeMenu);
+hamburgeropen.addEventListener('click', openMenu);
+for (let i = 0; i <= hamburgerListItem.length; i + 1) {
+  hamburgerListItem[i].addEventListener('click', closeMenu);
 }
