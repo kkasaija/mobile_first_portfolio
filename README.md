a91d39554c0f57d095a636be5fb2bb22a80ef8ef
Kasaija Kenneth <kasaijak@gmail.com>
2022-09-16 15:01:47 +0300

    Modify README.md, Index.html, Style.css and validation.js files

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ Reference Material:
 - HTML
 - CSS
 - Flexbox
-
+- Javascript
 
 ## Getting Started
 
@@ -63,7 +63,3 @@ learning and development process.
 ## 📝 License
 
 This project is [MIT](./LICENSE) licensed.
-
-_NOTE: we recommend using the [MIT license](https://choosealicense.com/licenses/mit/) - you can set it up quickly by [using templates available on GitHub](https://docs.github.com/en/communities/setting-up-your-project-for-healthy-contributions/adding-a-license-to-a-repository). You can also use [any other license](https://choosealicense.com/licenses/) if you wish._
-
-
--- validation.js
+++ validation.js
@@ -1,14 +1,14 @@
-const errorMessage = document.getElementById('#emailVal');
-const form = document.querySelector('form');
-const email = document.getElementById('#email');
-
+const form = document.getElementById('form');
+const email = document.getElementById('email');
+const errorMessage = document.getElementById('error');
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   if (email.value.toLowerCase() === email.value) {
     form.submit();
+    errorMessage.style.visibility = 'hidden';
   } else {
     errorMessage.innerHTML = 'email address must be lower case';
-    errorMessage.style.display = 'block';
+    errorMessage.style.visibility = 'visible';
+    errorMessage.style.color = 'green';
   }
 });
-
